9e49f91ee8e580803dda5987d0d478a07387c1be
Lforlana <mhmmdlanaaaa@gmail.com>
2025-05-13 16:46:12 -0700

    uts

--- C030324138UTS/menu.cpp
+++ C030324138UTS/menu.cpp
@@ -1,68 +1,152 @@
+#include <stdio.h>
 #include <iostream>
 #include <conio.h>
 using namespace std;
+int data[100000], t[100000];
+int n; // jumlah data
 
-void info(){
-  system ("cls");
-  cout<<"Indonesia Hebat, Poliban Jaya Jaya";
+// Pilihan Menu
+void dMenu()
+{
+  system("cls");
+  cout << "Aplikasi Tampilan Menu" << "\n";
+  cout << "1. Masukkan data" << "\n";
+  cout << "2. Tampilkan isi data" << "\n";
+  cout << "3. Sortir data" << "\n";
+  cout << "4. Informasi" << "\n";
+  cout << "5. Exit" << "\n";
+  cout << "Masukan angka :";
+}
+
+void ascendingSort( int data[], int n)
+{
+  int temp;
+  for (int i = 1; i < n; i++) {
+    temp = data[i];
+    int j = i - 1;
+    while (j >= 0 && data[j] > temp) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j + 1] = temp;
+  }
+}
+
+void descendingSort( int data[], int n)
+{
+  int temp;
+  for (int i = 1; i < n; i++) {
+    temp = data[i];
+    int j = i - 1;
+    while (j >= 0 && data[j] < temp) {
+      data[j + 1] = data[j];
+      j--;
+    }
+    data[j + 1] = temp;
+  }
+}
+
+void sort(int data[], int n) {
+  char choice;
+  cout << "Pilih urutan (a untuk ascending, d untuk descending): ";
+  cin >> choice;
+  if (choice == 'a') {
+    ascendingSort(data, n);
+  } else if (choice == 'd') {
+    descendingSort(data, n);
+  } else {
+    cout << "Pilihan tidak valid." << endl;
+  }
+}
+
+void copy() {
+  for (int i = 0; i < n; i++) {
+      t[i] = data[i];
+  }
+}
+
+// Fungsi setiap menu
+void ms()
+{
+  system("cls");
+  cout << "Masukkan jumlah data: ";
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << "Masukkan Data ke-" << i + 1 << ": ";
+      cin >> data[i];
+  }
+  copy();
+  cout << "Tekan sembarang tombol untuk kembali ke menu utama." << endl;
   getch();
 }
 
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Informasi"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
-}
-
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
-}
-
-
-int main() {
-char pl;
-do
+void md()
 {
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    info();
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
+  system("cls");
+  cout << "Data yang dimasukkan adalah: ";
+  for (int i = 0; i < n; i++) {
+    cout << t[i] << " ";
+}
+  cout << endl;
+  cout << "Tekan sembarang tombol untuk kembali ke menu utama." << endl;
+  getch();
+}
+
+void mt()
+{
+  system("cls");
+  sort(data, n);
+  cout << "Data yang telah diurutkan adalah: ";
+  for (int i = 0; i < n; i++) {
+    cout << data[i] << " ";
   }
 
+  cout << endl;
+  cout << "Tekan sembarang tombol untuk kembali ke menu utama." << endl;
+  getch();
+}
 
-} while (pl!='5');
-  return 0;
+void me()
+{
+  system("cls");
+  cout << "Pastikan data yang anda masukan sudah benar" << endl;
+  getch();
+}
+
+// Fungsi utama
+int main()
+{
+  char p;
+  do
+  {
+    dMenu();
+    p = getch();
+    switch (p)
+    {
+    case '1':
+      ms();
+      break;
+    case '2':
+      md();
+      break;
+    case '3':
+      mt();
+      break;
+    case '4':
+      me();
+      break;
+    case '5':
+      break;
+
+    default:
+      system("cls");
+      cout << "Pilihan Tidak Tersedia";
+      getch();
+      break;
+    }
+
+  } while (p != '5');
+ return 0;
 }
